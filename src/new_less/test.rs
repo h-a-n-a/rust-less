8ef18d28ec32ee6db0e92173c369df6664ac7172
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-01-08 16:16:59 +0800

    feat: add code

--- src/new_less/test.rs
+++ src/new_less/test.rs
@@ -1,15 +1,20 @@
 #[cfg(test)]
 mod tests {
+  use std::time::Instant;
   use crate::new_less::file::*;
   use crate::new_less::loc::LocMap;
   use crate::new_less::origin_parse::*;
   
   #[test]
   fn test_less() {
+    let now = Instant::now();
     let content = readfile(path_resolve("assets/demo.less")).unwrap();
     /// 转化 Less 文件中 最原始的 Block 片段
     let blocks = parse_origin_block(content).unwrap();
+    let end = now.elapsed();
     println!("{:#?}", blocks);
+    let a = end.as_micros() as f32;
+    println!("Running slow_function() took {} ms", a * 0.001);
     println!("........");
   }
   
