b955bd8bd566f6ddd8b16094d6368be205620708
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-01-15 01:37:40 +0800

    feat: add code

--- src/test.rs
+++ src/test.rs
@@ -1,8 +1,10 @@
 #[cfg(test)]
 mod tests {
   use std::cell::RefCell;
-  use std::ops::Deref;
+  use std::ops::{Deref, DerefMut};
   use std::rc::Rc;
+  use std::time::Duration;
+  use tokio::time::sleep;
   use crate::extend::string::StringExtend;
   use crate::extend::vec_str::VecStrExtend;
   
@@ -66,11 +68,69 @@ mod tests {
     }
   }
   
+  
   #[test]
   fn test_context_fn() {
     let a = Student::new();
     let b = a.new_classmate();
     println!(".......{}", b.classmate.unwrap().deref().borrow().name);
     println!(".......");
+  }
+  
+  async fn add(num: Rc<RefCell<i32>>, flag: String) {
+    let p = flag.parse::<i32>().unwrap();
+    if p > 0 && p % 2 == 0 {
+      sleep(Duration::from_secs(2)).await;
+    }
+    let mut i = 0;
+    while i < 10 {
+      *num.deref().borrow_mut().deref_mut() += 1;
+      i += 1;
+      let c = *num.deref().borrow().deref();
+      println!("flag is {} num is {}", flag, c);
+    }
+  }
+  
+  async fn exec_async(num: Rc<RefCell<i32>>) -> Rc<RefCell<i32>> {
+    let exec_times: i32 = 20;
+    let mut task_list = vec![];
+    let mut index = 0;
+    while index < exec_times {
+      let task = add(num.clone(), index.to_string());
+      task_list.push(task);
+      index += 1;
+    }
+    futures::future::join_all(task_list).await;
+    // let task1 = add(num.clone(), "a".to_string());
+    // let task2 = add(num.clone(), "b".to_string());
+    // futures::future::join(task2, task1).await;
+    num
+  }
+  
+  async fn main() {
+    sleep(Duration::from_secs(2)).await;
+    println!("100 ms have elapsed");
+  }
+  
+  #[test]
+  fn test_tokio() {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(main());
+  }
+  
+  #[test]
+  fn test_future() {
+    let num = Rc::new(RefCell::new(1));
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let d = rt.block_on(exec_async(num));
+    let c = *d.deref().borrow().deref();
+    println!("{}", c);
+    println!("........");
+    
+    // let mut a = 5;
+    // let mut value = Rc::new(RefCell::new(a));
+    // *value.deref().borrow_mut().deref_mut() += 10;
+    // let c = *value.deref().borrow().deref();
+    // println!(".......{}", c.clone().to_string());
   }
 }
