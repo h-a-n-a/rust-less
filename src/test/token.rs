5f8ba26c06ffc703bd0edf2d26c46873d68678a4
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-01-21 23:06:42 +0800

    feat: add code

--- src/test/token.rs
+++ src/test/token.rs
@@ -1,10 +1,9 @@
 use crate::extend::enum_extend::{EnumExtend};
 use crate::extend::vec_str::VecStrExtend;
-use crate::new_less::token::lib::Token;
-use crate::new_less::token::select::{SelectTokenParse, TokenSelect};
+use crate::new_less::token::select::{TokenSelect};
 
 #[test]
 fn test_enum_to_vec() {
   let list = TokenSelect::enum_vec();
-  assert_eq!(list.poly(), ".#[]*");
+  assert_eq!(list.poly(), ".#[]*:");
 }
