b20c0bf71e9d736e10562a0c6b834327736f5edd
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-01-24 18:36:05 +0800

    feat: add code

--- src/new_less/media.rs
+++ src/new_less/media.rs
@@ -3,7 +3,7 @@ use serde::{Serialize};
 use crate::extend::enum_extend::EnumExtend;
 use crate::extend::vec_str::VecStrExtend;
 use crate::new_less::token::lib::Token;
-use crate::new_less::token::media::TokenMeidaAllow;
+use crate::new_less::token::media::{TokenMediaLogic, TokenMediaType, TokenMeidaAllow};
 
 
 ///
@@ -16,6 +16,7 @@ pub struct MediaQuery {
 }
 
 impl MediaQuery {
+  
   ///
   /// 初始化方法
   ///
@@ -46,12 +47,23 @@ impl MediaQuery {
     self.origin_txt.clone()
   }
   
+  ///
+  /// 子转化 媒体功能
+  ///
+  pub fn parse_media_feature(&mut self, start: &usize) -> Result<(String, usize), String> {
+    Ok(("".to_string(), 0))
+  }
   
+  ///
+  /// 转化代码
+  ///
   pub fn parse(&mut self) -> Result<(), String> {
     let charlist = &self.charlist;
     let mut temp: String = "".to_string();
     
-    if charlist.len() < 6 || charlist[0..6].poly().as_str() != "@media" {
+    if charlist.len() < 6 ||
+      (charlist.len() == 6 && charlist[0..6].poly().as_str() != "@media") ||
+      (charlist.len() > 6 && charlist[0..7].poly().as_str() != "@media ") {
       return Err(format!("select_txt not match media query"));
     }
     let mut word_vec = vec!["@media".to_string()];
@@ -60,14 +72,30 @@ impl MediaQuery {
     // 循环解析
     while index < charlist.len() {
       let char = charlist.get(index).unwrap().to_string();
+      let nextchar = if index == charlist.len() - 1 {
+        "".to_string()
+      } else {
+        charlist.get(index + 1).unwrap().to_string()
+      };
       if Token::is_token(&char) {
         if Token::is_space_token(&char) {
-          // todo! 验收词
-          word_vec.push(temp.clone())
+          if !Token::is_space_token(&nextchar) {
+            let word = temp.clone();
+            if TokenMediaLogic::is(&word) || TokenMediaType::is(&word) {
+              word_vec.push(temp.clone());
+            } else {
+              return Err(self.errormsg(&index).err().unwrap());
+            }
+          } else {
+            index += 1;
+            continue;
+          }
         } else if TokenMeidaAllow::is(&char) {
           match TokenMeidaAllow::try_from(char.as_str()).unwrap() {
             TokenMeidaAllow::LeftBrackets => {
               // todo ! 跳变分析 feature
+              
+              
             }
             _ => {
               return Err(self.errormsg(&index).err().unwrap());
