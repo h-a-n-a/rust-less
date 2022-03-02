39c920559024cc7c3105653f425e727279051601
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-03-02 23:31:56 +0800

    feat: add code

--- src/unit_test/render.rs
+++ src/unit_test/render.rs
@@ -17,11 +17,11 @@ fn test_less_render() {
 fn test_less_render_without_sourcemap() {
   let record = wastetime("test_less_render");
   let filepath = path_resolve("assets/var.less");
-  let context = Context::new(ParseOption{
+  let context = Context::new(ParseOption {
     include_path: vec![],
     sourcemap: false,
     tabspaces: 2,
-    hooks: Default::default()
+    hooks: Default::default(),
   }, Some(filepath.clone())).unwrap();
   let res = context.render(filepath).unwrap();
   println!("{}", res);
@@ -31,10 +31,23 @@ fn test_less_render_without_sourcemap() {
 #[test]
 fn test_less_arco_pro_render() {
   let record = wastetime("test_less_render");
-  let index = 42;
-  let filepath = path_resolve(format!("assets/arco-pro/{}.less", index).as_str());
-  let context = Context::new(Default::default(), Some(filepath.clone())).unwrap();
-  let res = context.render(filepath).unwrap();
-  println!("{}", res);
+  let mut index = 1;
+  let mut error_file = vec![];
+  while index < 39 {
+    let filepath = path_resolve(format!("assets/arco-pro/{}.less", index).as_str());
+    let context = Context::new(Default::default(), Some(filepath.clone())).unwrap();
+    let res = match context.render(filepath.clone()) {
+      Ok(res) => {
+        res
+      }
+      Err(msg) => {
+        error_file.push(filepath);
+        msg
+      }
+    };
+    println!("{}", res);
+    index += 1;
+  }
+  println!("{:#?}", error_file);
   record();
 }
