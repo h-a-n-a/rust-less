c90503e498a0473f788fb3152b07b450cb18746e
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-03-04 15:05:37 +0800

    feat: add code

--- src/example/detail.rs
+++ src/example/detail.rs
@@ -1,3 +1,4 @@
+use crate::extend::string::StringExtend;
 use crate::new_less::context::Context;
 use crate::new_less::var_node::VarNode;
 use test::Bencher;
@@ -10,6 +11,51 @@ fn parse_value_bench(bench: &mut Bencher) {
       let content = r#"@width:400px;"#.to_string();
       VarNode::new(content, None, None, None, Context::default());
       index += 1;
+    }
+  });
+}
+
+#[bench]
+fn parse_number_bench(bench: &mut Bencher) {
+  bench.iter(|| {
+    let is_number = |char: &str| -> bool { char.parse::<i32>().is_ok() };
+    let list = vec!["a", "1"];
+    list.iter().for_each(|x| {
+      is_number(x);
+    });
+  });
+}
+
+#[bench]
+fn parse_number_compare_bench(bench: &mut Bencher) {
+  bench.iter(|| {
+    let is_number = |char: &str| -> bool {
+      vec!["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].contains(&char)
+    };
+    let list = vec!["a", "1"];
+    list.iter().for_each(|x| {
+      is_number(x);
+    });
+  });
+}
+
+#[bench]
+fn parse_str_u32_bench(bench: &mut Bencher) {
+  bench.iter(|| {
+    let char = "0123456789";
+    let charlist = char.chars().map(|x| x as u32).collect::<Vec<u32>>();
+    for item in charlist {
+      println!("{:#?}", item);
+    }
+  });
+}
+
+#[bench]
+fn parse_str_bench(bench: &mut Bencher) {
+  bench.iter(|| {
+    let chars = "0123456789";
+    for item in chars.to_string().tocharlist() {
+      println!("{:#?}", item);
     }
   });
 }
