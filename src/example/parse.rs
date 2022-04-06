2e9e9d94e0554168cd2c06f4e49a4f6bc5b2abf8
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-04-06 20:23:16 +0800

    feat: add code

--- src/example/parse.rs
+++ src/example/parse.rs
@@ -17,7 +17,16 @@ fn parse_less_bench(bench: &mut Bencher) {
 fn parse_var_bench(bench: &mut Bencher) {
   bench.iter(|| {
     let filepath = path_resolve("assets/var.less");
-    let context = Context::new(Default::default(), Some(filepath.clone())).unwrap();
+    let context = Context::new(
+      ParseOption {
+        include_path: vec![],
+        sourcemap: false,
+        tabspaces: 2,
+        hooks: Default::default(),
+      },
+      Some(filepath.clone()),
+    )
+    .unwrap();
     context.render(filepath).unwrap();
   });
 }
