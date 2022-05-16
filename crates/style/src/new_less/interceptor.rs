1f6acf85b656b54df708d453b8610a47b1696af7
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-05-16 23:08:32 +0800

    feat: support less.js interceptor

--- crates/style/src/new_less/interceptor.rs
+++ crates/style/src/new_less/interceptor.rs
@@ -1,12 +1,62 @@
 use serde_json::Value;
 use std::collections::HashMap;
+use std::{env, fs};
 use std::path::Path;
 use std::process::{Command, Stdio};
 use crate::new_less::file::path_resolve;
 
 pub struct LessInterceptor;
 
+
 impl LessInterceptor {
+  ///
+  /// 复制js-lib
+  /// 检查有文件的话 不进行调用
+  ///
+  fn copy_js_lib(target_dir: &str) {
+    // 清空dir
+    let command = format!("rm -rf {}/**", target_dir);
+    let mut rm_task = Command::new("sh");
+    rm_task.arg("-c").arg(command);
+    rm_task.current_dir(target_dir).status().unwrap();
+    // 拷贝js-lib
+    let js_lib_dir = format!("{}/**", path_resolve("js-lib"));
+    let command = format!("cp -rf {} {}", js_lib_dir, target_dir);
+    let mut cp_task = Command::new("sh");
+    cp_task
+      .arg("-c")
+      .arg(command);
+    cp_task.current_dir(target_dir).status().unwrap();
+  }
+
+  ///
+  /// js-lib 文件管理
+  /// 用来支持 less.js cwd require.resolve js-plugin
+  ///
+  fn filemanger() -> Result<String, String> {
+    let cwd = env::current_dir().unwrap();
+    let temp_rspack_style_dir = cwd.join(".rspack-style");
+    let js_main_file = temp_rspack_style_dir.join("dist/main.js");
+
+    return if temp_rspack_style_dir.exists() && temp_rspack_style_dir.is_dir() {
+      if js_main_file.exists() {
+        // 主文件存在 则返回
+        Ok(js_main_file.into_os_string().into_string().unwrap())
+      } else {
+        // 主文件不存在 则 从 自己 js-lib 中进行复制
+        Self::copy_js_lib(temp_rspack_style_dir.to_str().unwrap());
+        Ok(js_main_file.into_os_string().into_string().unwrap())
+      }
+    } else if !temp_rspack_style_dir.exists() {
+      fs::create_dir_all(temp_rspack_style_dir.clone()).unwrap();
+      Self::copy_js_lib(temp_rspack_style_dir.to_str().unwrap());
+      Ok(js_main_file.into_os_string().into_string().unwrap())
+    } else {
+      // maybe .rspack-style is link or file
+      Err(format!("rspack-style LessInterceptor filemanger call ->  {} must be dir", temp_rspack_style_dir.into_os_string().into_string().unwrap()))
+    }
+  }
+
   pub fn handle(filepath: &str, content: &str) -> Result<String, String> {
     let path = Path::new(filepath);
     if path.extension() == Some("less".to_string().as_ref()) {
@@ -29,7 +79,7 @@ impl LessInterceptor {
       content_map.insert("content".to_string(), content.to_string());
       let content_arg = serde_json::to_string(&content_map).unwrap();
       let option_arg = serde_json::to_string(&option_map).unwrap();
-      let js_file = path_resolve("js-lib/dist/main.js");
+      let js_file = Self::filemanger()?;
       let mut task = Command::new("node");
       task.arg(js_file.as_str());
       task.arg("--content");
@@ -39,7 +89,7 @@ impl LessInterceptor {
       task.current_dir(cwd);
       task.stdout(Stdio::piped());
 
-      let _test_node_content = format!("node {} --content {} --option {}", js_file, content_arg, option_arg);
+      // let _test_node_content = format!("node {} --content {} --option {}", js_file, content_arg, option_arg);
 
       let output = task
         .output()
--- crates/style/src/test/hook.rs
+++ crates/style/src/test/hook.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+use std::process::Command;
 use crate::new_less::applicationn::Application;
 use crate::new_less::file::path_resolve;
 use std::sync::Arc;
@@ -20,4 +22,35 @@ fn test_less_content_hook() {
   let app = Application::default();
   let res = app.render(filepath.as_str()).unwrap();
   println!("rust_res: -> \n {}", res);
+}
+
+
+#[test]
+fn test_js_lib_copy() {
+  ///
+  /// 复制js-lib
+  /// 检查有文件的话 不进行调用
+  ///
+  fn copy_js_lib(target_dir: &str) {
+    // 清空dir
+    let command = format!("rm -rf {}/**", target_dir);
+    let mut rm_task = Command::new("sh");
+    rm_task.arg("-c").arg(command);
+    rm_task.current_dir(target_dir).status().unwrap();
+    // 拷贝js-lib
+    let js_lib_dir = format!("{}/**", path_resolve("js-lib"));
+    let command = format!("cp -rf {} {}", js_lib_dir, target_dir);
+    let mut cp_task = Command::new("sh");
+    cp_task
+      .arg("-c")
+      .arg(command);
+    cp_task.current_dir(target_dir).status().unwrap();
+  }
+
+  let target_dir = path_resolve(".rspack-style");
+  copy_js_lib(target_dir.as_str());
+
+  let res = Path::new(path_resolve(".rspack-style/dist/main.js").as_str()).exists();
+
+  assert_eq!(res, true);
 }
