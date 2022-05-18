93b76a41da701f73499c2718439c7604283de929
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-05-18 17:37:17 +0800

    feat: support less.js interceptor

--- crates/style/src/new_less/filenode.rs
+++ crates/style/src/new_less/filenode.rs
@@ -143,8 +143,8 @@ impl FileNode {
       } else {
         item.to_string()
       };
-      let value = format!(".{}_{}", key, self.info.borrow().hash_perfix);
-      res += format!(r#"{}: "{}","#, item, value).as_str();
+      let value = format!(".{}_{}", item, self.info.borrow().hash_perfix);
+      res += format!(r#"{}: "{}","#, key, value).as_str();
       res += " \n";
     }
     res
